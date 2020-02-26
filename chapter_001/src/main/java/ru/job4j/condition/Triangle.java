a12a98cb0b4d9125d5fc98fee773477b1c9ebf48
Vladimir <lazer_shtamp@mail.ru>
2020-02-26 21:31:32 +0300

    "4.3. А существует ли треугольник?[#241333]: add ru.job4j.condition.Triangle and ru.job4j.condition.TriangleTest"

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+//  ab + ac > bc и ac + bc > ab и ab + bc > ac - в этом случае треугольник существует
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return (ab+ac > bc) && (ac+bc > ab) && (ab+bc > ac);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenExist() {
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotExist() {
+        boolean result = Triangle.exist(0.9, 0.9, 2.0);
+        assertThat(result, is(false));
+    }
+}
