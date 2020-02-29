b449a10e9ac84fd3ed20714ff86d4caec27acde3
Vladimir <lazer_shtamp@mail.ru>
2020-02-29 23:28:09 +0300

    "6.3. Массив заполнен true или false[#241340]: add class ru.job4j.array.Check, add class ru.job4j.array.CheckTest"

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 0; index < data.length; index++) {
+            if (data[0] != data[index]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = Check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataMonoByFalseThenFalse() {
+        boolean[] input = new boolean[] {false, false, false};
+        boolean result = Check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = Check.mono(input);
+        assertThat(result, is(false));
+    }
+}
