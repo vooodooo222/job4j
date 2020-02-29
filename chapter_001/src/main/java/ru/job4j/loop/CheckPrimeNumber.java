7391557e77467f59bf6c2655435f457e6cbd323a
Vladimir <lazer_shtamp@mail.ru>
2020-02-29 16:24:12 +0300

    "5.5. Простое число.[#241410]: fix class ru.job4j.loop.CheckPrimeNumber"

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,12 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean isCanPrimeNumber = number >= 2;
-        if (isCanPrimeNumber) {
-            for (int i = 2; i != number; i++) {
-                if ((number % i) == 0) {
-                    isCanPrimeNumber = false;
-                    break;
-                }
+        for (int i = 2; isCanPrimeNumber && i != number; i++) {
+            if ((number % i) == 0) {
+                isCanPrimeNumber = false;
+                break;
             }
         }
         return isCanPrimeNumber;
