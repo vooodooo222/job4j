a6e38b7dac17ce2dd13047cf7021085ff69ce4b1
Vladimir <lazer_shtamp@mail.ru>
2020-02-29 17:54:57 +0300

    "6.0.2. Заполнение массива.[#241351]: update class ru.job4j.array.ArrayDefinition"

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Vasya Petrov";
+        names[1] = "Sasha Sidorov";
+        names[2] = "Sidor Vasechkin";
+        names[3] = "Tolya Ivanov";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
