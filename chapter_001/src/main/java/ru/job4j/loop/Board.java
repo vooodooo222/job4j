37f6231565b08c9065596b2f2a8e214aac23665c
Vladimir <lazer_shtamp@mail.ru>
2020-03-01 11:45:08 +0300

    "5.3. Построить шахматную доску в псевдографике.[#241418]: fix class ru.job4j.loop.Board method paint"

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -4,10 +4,10 @@ public class Board {
     public static void paint(int width, int height) {
         for (int row = 0; row < height; row++) {
             for (int cell = 0; cell < width; cell++) {
-                if (cell % 2 == 0) {
-                    System.out.print(row % 2 == 0? "X" : "O");
+                if((row + cell) % 2 == 0 ) {
+                    System.out.print("X");
                 } else {
-                    System.out.print(row % 2 == 0? "O" : "X");
+                    System.out.print("O");
                 }
             }
             // добавляем перевод на новую строку.
