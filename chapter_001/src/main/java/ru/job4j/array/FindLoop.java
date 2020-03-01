d3c656e49640a8b68bd3f39a3f2af846ae9e8b8c
Vladimir <lazer_shtamp@mail.ru>
2020-03-01 11:35:09 +0300

    "6.5. Поиск индекса в диапазоне.[#241344]: update class ru.job4j.array.FindLoop, update class ru.job4j.array.FindLoopTest"

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -2,13 +2,30 @@ package ru.job4j.array;
 
 public class FindLoop {
     public int indexOf(int[] data, int el) {
-        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        int indexFound = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
-                rst = index;
+                indexFound = index;
                 break;
             }
         }
-        return rst;
+        return indexFound;
+    }
+
+    public int indexOf(int[] data, int el, int startIndex, int finishIndex) {
+        int indexFound = -1; // если элемента нет в массиве, то возвращаем -1.
+        if (isValid(startIndex, data) && isValid(finishIndex, data)) {
+            for (int index = startIndex; index < finishIndex; index++) {
+                if (data[index] == el) {
+                    indexFound = index;
+                    break;
+                }
+            }
+        }
+        return indexFound;
+    }
+
+    public boolean isValid(int index, int[] data) {
+        return (index >= 0 && index < data.length);
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -6,22 +6,70 @@ import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
     @Test
-    public void whenArrayHasNot5Then0() {
+    public void whenArrayHasNot5ThenNegative() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {4, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
-        int expect = -1;
-        assertThat(result, is(expect));
+        int indexFound = find.indexOf(input, value);
+        int indexExpect = -1;
+        assertThat(indexFound, is(indexExpect));
     }
 
     @Test
-    public void whenArrayHas5Then1() {
+    public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
-        int expect = 0;
-        assertThat(result, is(expect));
+        int indexFound = find.indexOf(input, value);
+        int indexExpect = 0;
+        assertThat(indexFound, is(indexExpect));
+    }
+
+    @Test
+    public void whenFind3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int startIndex = 2;
+        int finishIndex = 4;
+        int indexFound = find.indexOf(input, value, startIndex, finishIndex);
+        int indexExpect = 3;
+        assertThat(indexFound, is(indexExpect));
+    }
+
+    @Test
+    public void whenStartIndexOutOfBoundsThenNegative() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int startIndex = -1;
+        int finishIndex = 4;
+        int indexFound = find.indexOf(input, value, startIndex, finishIndex);
+        int indexExpect = -1;
+        assertThat(indexFound, is(indexExpect));
+    }
+
+    @Test
+    public void whenFinishIndexOutOfBoundsThenNegative() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int startIndex = 2;
+        int finishIndex = 6;
+        int indexFound = find.indexOf(input, value, startIndex, finishIndex);
+        int indexExpect = -1;
+        assertThat(indexFound, is(indexExpect));
+    }
+
+    @Test
+    public void whenArrayIsEmptyThenNegative() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {};
+        int value = 2;
+        int startIndex = 0;
+        int finishIndex = 1;
+        int indexFound = find.indexOf(input, value, startIndex, finishIndex);
+        int indexExpect = -1;
+        assertThat(indexFound, is(indexExpect));
     }
 }
