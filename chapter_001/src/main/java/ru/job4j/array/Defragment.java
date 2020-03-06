d8112022572aed05fa726ae1ff5a88aea64b7bc0
Vladimir <lazer_shtamp@mail.ru>
2020-03-06 20:58:16 +0300

    "6.8. Дефрагментация массива.[#241358]: add class ru.job4j.array.Defragment, add class ru.job4j.array.DefragmentTest"

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int nextIndex = index + 1; nextIndex < array.length; nextIndex++) {
+                    if (array[nextIndex] != null) {
+                        // меняем местами null с не null
+                        String nullCell = array[index];
+                        array[index] = array[nextIndex];
+                        array[nextIndex] = nullCell;
+                        break;
+                    }
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
