7d36c07ea9be2e81c06e17b77faeac7a8763a5ae
Vladimir <lazer_shtamp@mail.ru>
2020-03-16 21:37:14 +0300

    1.5. Поля объекта[#241507]: update class ru.job4j.oop.Cat - add new methods

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -1,14 +1,34 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eaten " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        String nickName = "gav";
+        System.out.println("There are " + nickName + "'s food.");
+        Cat gav = new Cat();
+        gav.giveNick(nickName);
+        gav.eat("kotleta");
+        gav.show();
+        nickName = "black";
+        System.out.println("There are " + nickName + "'s food.");
+        Cat black = new Cat();
+        black.giveNick(nickName);
+        black.eat("fish");
+        black.show();
     }
 }
