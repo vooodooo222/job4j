bc643592844dd5697c537454b70cb2327a967299
Vladimir <lazer_shtamp@mail.ru>
2020-03-19 20:27:11 +0300

    3. Перегрузить метод max для трех чисел[#241505]: update class ru.job4j.condition.Max - add new overloaded methods max

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,7 +2,14 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+        return left > right ? left : right;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(max(first, second), third);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(max(max(first, second), third), fourth);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -22,4 +22,64 @@ public class MaxTest {
         int result = Max.max(2, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenMax2To3To4Then4() {
+        int result = Max.max(2, 3, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax4To3To2Then2() {
+        int result = Max.max(4, 3, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax3To4To2Then4() {
+        int result = Max.max(3, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax2To4To3Then2() {
+        int result = Max.max(2, 4, 3);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax2To3To4To5Then4() {
+        int result = Max.max(2, 3, 4, 5);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax5To4To3To2Then2() {
+        int result = Max.max(5, 4, 3, 2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax3To5To4To2Then4() {
+        int result = Max.max(3, 5, 4, 2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax2To5To4To3Then2() {
+        int result = Max.max(2, 5, 4, 3);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax3To4To5To2Then4() {
+        int result = Max.max(3, 4, 5, 2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax2To4To5To3Then2() {
+        int result = Max.max(2, 4, 5, 3);
+        assertThat(result, is(5));
+    }
 }
