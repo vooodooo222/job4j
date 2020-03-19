77d67eeb451ce32b7c3f8af0cd6b9a415673e279
Vladimir <lazer_shtamp@mail.ru>
2020-03-19 15:55:35 +0300

    2.2. Рефакторинг - Площадь треугольника.[#241503]: update class ru.job4j.condition.Triangle and ru.job4j.condition.TriangleTest - code refactoring and add JavaDocs

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,9 +1,84 @@
 package ru.job4j.condition;
 
-//  ab + ac > bc и ac + bc > ab и ab + bc > ac - в этом случае треугольник существует
-
+/**
+ * Triangle
+ * @author Aleksandrov Vladimir (lazer_shtamp@mail.ru)
+ */
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
+    /**
+     * Точка A
+     */
+    private Point a;
+
+    /**
+     * Точка B
+     */
+    private Point b;
+
+    /**
+     * Точка C
+     */
+    private Point c;
+
+    /**
+     * Конструктор класса Triangle
+     * @param a - точка A
+     * @param b - точка B
+     * @param c - точка C
+     */
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод вычисления полупериметра треугольника по длинам сторон.
+     *
+     * Формула.
+     *
+     * p = (ab + bc + ac) / 2
+     *
+     * @param ab расстояние между точками a b
+     * @param bc расстояние между точками b c
+     * @param ac расстояние между точками a c
+     * @return Периметр.
+     */
+    public double halfPerimeter(double ab, double bc, double ac) {
+        return (ab + bc + ac) / 2;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула Герона.
+     *
+     * S = √ p *(p - ab) * (p - bc) * (p - ac)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = a.distance(b);
+        double bc = b.distance(c);
+        double ac = a.distance(c);
+        double p = halfPerimeter(ab, bc, ac);
+        if (this.exist(ab, bc, ac)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     * @param ab расстояние между точками a b
+     * @param bc расстояние между точками b c
+     * @param ac расстояние между точками a c
+     * @return true в случае успеха, иначе false
+     */
+    protected boolean exist(double ab, double bc, double ac) {
         return (ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,19 +1,64 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
+    public void validAreaOfTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(3, 0);
+        Point c = new Point(0, 6);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        Assert.assertEquals(8.99, result, 0.01);
+    }
+
+    @Test
+    public void notValidAreaOfTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(4, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        Assert.assertEquals(-1, result, 0.01);
+    }
+
+    @Test
+    public void returnHalfPerimeterOfTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(3, 0);
+        Point c = new Point(0, 6);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.halfPerimeter(a.distance(b), b.distance(c), a.distance(c));
+        Assert.assertEquals(7.85, result, 0.01);
+    }
+
+    @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(0, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double ab = a.distance(b);
+        double bc = b.distance(c);
+        double ac = a.distance(c);
+        boolean result = triangle.exist(ab, bc, ac);
         assertThat(result, is(true));
     }
 
     @Test
     public void whenNotExist() {
-        boolean result = Triangle.exist(0.9, 0.9, 2.0);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(4, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double ab = a.distance(b);
+        double bc = b.distance(c);
+        double ac = a.distance(c);
+        boolean result = triangle.exist(ab, bc, ac);
         assertThat(result, is(false));
     }
 }
