b3550e7722b014f5d985ad82447fe668ca132079
Vladimir <lazer_shtamp@mail.ru>
2020-03-19 20:13:44 +0300

    2.2. Рефакторинг - Площадь треугольника.[#241503]: update class ru.job4j.calculator.Calculator and add JavaDocs

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,15 +2,36 @@ package ru.job4j.calculator;
 
 /**
  * Calculator
- * Contains and performs arithmetic operations
+ * Содержит и выполняет арифметические операции
  * @author Vladimir Aleksandrov (lazer_shtamp@mail.ru)
  */
 public class Calculator {
 
     /**
-     * plus.
-     * @param first - first value of expression
-     * @param second - second value of expression
+     * Складывает два входных параметра с плавающей точкой
+     * @param first - перое число
+     * @param second - второе число
+     * @return Возвращает результат с плавающей точкой
+     */
+    public double add(double first, double second) {
+        return first + second;
+    }
+
+    /**
+     * Складывает три входных параметра с плавающей точкой
+     * @param first - перое число
+     * @param second - второе число
+     * @param third - третье число
+     * @return Возвращает результат с плавающей точкой
+     */
+    public double add(double first, double second, double third) {
+        return first + second + third;
+    }
+
+    /**
+     * Выводит в консоль результат сложения двух целочисленных входных параметров
+     * @param first - перое число
+     * @param second - второе число
      */
     public static void plus(int first, int second) {
         int result = first + second;
@@ -18,7 +39,7 @@ public class Calculator {
     }
 
     /**
-     * main.
+     * Вывод в консоль результатов значений арифметических операций
      * @param args - input parameter of program
      */
     public static void main(String[] args) {
@@ -26,7 +47,6 @@ public class Calculator {
         int two = 2;
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
-
         int six = 6;
         int four = 4;
         int five = 5;
@@ -36,12 +56,10 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
-
         int size = 10;
         size = 100;
         size = size - 5;
         System.out.println(size);
-
         Calculator.plus(100, 500);
         Calculator.plus(4, 2);
         Calculator.plus(3, 5);
