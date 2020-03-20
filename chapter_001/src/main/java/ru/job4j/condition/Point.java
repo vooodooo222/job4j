8667173fafc1ffe2c64e2e6571bd1a9b54680887
Vladimir <lazer_shtamp@mail.ru>
2020-03-20 11:49:24 +0300

    4. Расстояние между точками в трехмерном пространстве.[#241504]: update class ru.job4j.condition.Point and class ru.job4j.condition.PointTest - added new overloaded constructor with supports 3D for class Point and new test

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -16,7 +16,12 @@ public class Point {
     private int y;
 
     /**
-     * Конструктор класса Point
+     * Координата Z
+     */
+    private int z;
+
+    /**
+     * Конструктор класса Point для двумерной плоскости
      * @param x - координата X
      * @param y - координата Y
      */
@@ -26,12 +31,33 @@ public class Point {
     }
 
     /**
+     * Конструктор класса Point для трехмерного пространства
+     * @param x - координата X
+     * @param y - координата Y
+     * @param z - координата Z
+     */
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
      * Считает дистанцию между текущей точкой и указанной точкой во входном параметре
      * @param that - точка 2
      * @return значение дистанции между текущей точкой и указанной точкой во входном параметре
      */
     public double distance(Point that) {
         return Math.sqrt(Math.pow((that.x - this.x), 2) + Math.pow((that.y - this.y), 2));
+    }
+
+    /**
+     * Считает дистанцию между текущей точкой и указанной точкой во входном параметре в трехмерном пространстве
+     * @param that - точка 2 в трехмерном пространстве
+     * @return значение дистанции между текущей точкой и указанной точкой во входном параметре
+     */
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow((that.x - this.x), 2) + Math.pow((that.y - this.y), 2) + Math.pow((that.z - this.z), 2));
     }
 
     /**
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -11,4 +11,12 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertEquals(3.6, out, 0.1);
     }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(3, 2, 1);
+        Point b = new Point(5, 5, 2);
+        double out = a.distance3d(b);
+        Assert.assertEquals(3.74, out, 0.01);
+    }
 }
