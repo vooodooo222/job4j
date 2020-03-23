b58e34c9384909741295b6247ad40a787025f310
Vladimir <lazer_shtamp@mail.ru>
2020-03-23 10:07:35 +0300

    4. Переопределение[#241519]: added class JSONReport and TextReport - implemented functional of overriding of method

---
+++ chapter_002/src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop.inheritance;
+
+public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String body) {
+        return "{"
+                + System.lineSeparator()
+                + System.lineSeparator()
+                + "  name : " + name + ","
+                + System.lineSeparator()
+                + "  body : " + body + ","
+                + System.lineSeparator()
+                + System.lineSeparator()
+                + "}";
+    }
+}
