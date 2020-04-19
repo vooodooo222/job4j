1e6ae0fdc2214abf77a1fcaf07313ecead682539
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-04-19 22:23:34 +0300

    0. Сортировка[#241438]: examples

---
+++ chapter_003/src/main/java/ru/job4j/collection/sort/ListSort.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection.sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSort {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 3, 4, 1, 2);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Integer first = 1;
+        Integer second = 2;
+        int rsl = first.compareTo(second);
+        System.out.println(rsl);
+        String petr = "Petr";
+        String ivan = "Ivan";
+        int rslStr = petr.compareTo(ivan);
+        System.out.println(rslStr);
+        list = Arrays.asList(5, 3, 4, 1, 2);
+        System.out.println(list);
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
+    }
+}
