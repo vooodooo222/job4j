7c29c730de6c9cb7a04eb1d0f7a5bbca28209d4f
vooodooo222 <lazer_shtamp@mail.ru>
2020-04-24 08:38:26 +0300

    1. Организовать сортировку User[#241436]: step 1

---
+++ chapter_003/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int rsl = this.getName().compareTo(o.getName());
+        return rsl != 0 ? rsl : Integer.compare(this.getAge(), o.getAge());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
