31c2d99e08e96d394af17b9b9251248edfa82f83
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-04-30 23:29:43 +0300

    4. Сортировка номера. Refactoring 1.

--- chapter_003/src/main/java/ru/job4j/collection/sort/LexSort.java
+++ chapter_003/src/main/java/ru/job4j/collection/sort/LexSort.java
@@ -1,45 +1,68 @@
 package ru.job4j.collection.sort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
-        List<Integer> leftList = getListFirstDigitsByDots(left);
-        List<Integer> rightList = getListFirstDigitsByDots(right);
-        for (int index = 0; index < Math.min(leftList.size(), rightList.size()); index++) {
-            rsl = leftList.get(index).compareTo(rightList.get(index));
-            if (rsl != 0) {
-                break;
+        String[] leftSplit = left.split("\\.");
+        String[] rightSplit = right.split("\\.");
+        for (int i = 0; i < Math.min(leftSplit.length, rightSplit.length); i++) {
+            char chL = leftSplit[i].charAt(0);
+            char chR = rightSplit[i].charAt(0);
+            int cmp = 0;
+            if (chL >= '0' && chL <= '9' && chR >= '0' && chR <= '9') {
+                try {
+                    cmp = Integer.valueOf(leftSplit[i]).compareTo(
+                            Integer.valueOf(rightSplit[i]));
+                } catch (NumberFormatException nfe) {
+                    nfe.getStackTrace();
+                }
+            }
+            if (cmp == 0) {
+                cmp = leftSplit[i].compareTo(rightSplit[i]);
+            }
+            if (cmp != 0) {
+                return cmp;
             }
         }
-        if (rsl == 0) {
-            int compareBySizeList = Integer.compare(leftList.size(), rightList.size());
-            rsl = compareBySizeList == 0 ? left.compareTo(right) : compareBySizeList;
-        }
-        return rsl;
+        return leftSplit.length - rightSplit.length;
     }
 
-    private List<Integer> getListFirstDigitsByDots(String string) {
-        List<Integer> list = new ArrayList<>();
-        int beginIndex = 0;
-        int endIndex;
-        do {
-            endIndex = string.indexOf('.', beginIndex);
-            if (endIndex < 0) {
-                break;
-            }
-            String str = string.substring(beginIndex, endIndex);
-            if (!str.chars().allMatch(Character::isDigit)) {
-                break;
-            }
-            list.add(Integer.valueOf(str));
-            beginIndex = endIndex + 1;
-        } while (true);
-        return list;
-    }
+// other variants:
+//    public int compare(String left, String right) {
+//        int rsl = 0;
+//        List<Integer> leftList = getListFirstDigitsByDots(left);
+//        List<Integer> rightList = getListFirstDigitsByDots(right);
+//        for (int index = 0; index < Math.min(leftList.size(), rightList.size()); index++) {
+//            rsl = leftList.get(index).compareTo(rightList.get(index));
+//            if (rsl != 0) {
+//                break;
+//            }
+//        }
+//        if (rsl == 0) {
+//            int compareBySizeList = Integer.compare(leftList.size(), rightList.size());
+//            rsl = compareBySizeList == 0 ? left.compareTo(right) : compareBySizeList;
+//        }
+//        return rsl;
+//    }
+//
+//    private List<Integer> getListFirstDigitsByDots(String string) {
+//        List<Integer> list = new ArrayList<>();
+//        int beginIndex = 0;
+//        int endIndex;
+//        do {
+//            endIndex = string.indexOf('.', beginIndex);
+//            if (endIndex < 0) {
+//                break;
+//            }
+//            String str = string.substring(beginIndex, endIndex);
+//            if (!str.chars().allMatch(Character::isDigit)) {
+//                break;
+//            }
+//            list.add(Integer.valueOf(str));
+//            beginIndex = endIndex + 1;
+//        } while (true);
+//        return list;
+//    }
 }
--- chapter_003/src/test/java/ru/job4j/collection/sort/LexSortTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/sort/LexSortTest.java
@@ -12,10 +12,21 @@ public class LexSortTest {
                 "100.1.2. Task.B",
                 "10.1. Task.",
                 "1. Task.",
+                "100.1.2. Tas",
+                "Z2.2",
                 "2.1. TaskA.",
+                "Z2",
+                "MN",
                 "2.2. Task.",
                 "2.1. TaskB.",
+                "Z1",
+                "100.1.2. Task.2.B",
+                "AZ",
                 "100.1.2. Task.A",
+                "NM",
+                "100.1.2. Task.2.A",
+                "Z2.1",
+                "100.1.2. Tas",
                 "20. Task."
         };
         String[] out = {
@@ -25,8 +36,38 @@ public class LexSortTest {
                 "2.2. Task.",
                 "10.1. Task.",
                 "20. Task.",
+                "100.1.2. Tas",
+                "100.1.2. Tas",
+                "100.1.2. Task.2.A",
+                "100.1.2. Task.2.B",
                 "100.1.2. Task.A",
-                "100.1.2. Task.B"
+                "100.1.2. Task.B",
+                "AZ",
+                "MN",
+                "NM",
+                "Z1",
+                "Z2",
+                "Z2.1",
+                "Z2.2"
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+
+
+    @Test
+    public void sortIncorrectNum() {
+        String[] input = {
+                "1zz.1.2. Tas",
+                "1aa.1.2. Tas",
+                "1z2.1.2. Tas",
+                "1z10.1.2. Tas",
+        };
+        String[] out = {
+                "1aa.1.2. Tas",
+                "1z10.1.2. Tas",
+                "1z2.1.2. Tas",
+                "1zz.1.2. Tas",
         };
         Arrays.sort(input, new LexSort());
         assertThat(input, is(out));
