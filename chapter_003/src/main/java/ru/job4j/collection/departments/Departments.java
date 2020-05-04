05cd74f9fa2beeec812884a993886ec754fb498f
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-05-04 13:30:31 +0300

    Отсортировать департаменты [#241431]: added check performance when use TreeSet and HashSet, refactoring

--- chapter_003/src/main/java/ru/job4j/collection/departments/Departments.java
+++ chapter_003/src/main/java/ru/job4j/collection/departments/Departments.java
@@ -3,7 +3,13 @@ package ru.job4j.collection.departments;
 import java.util.*;
 
 public class Departments {
-    public static <T> List<String> fillGaps(List<String> deps, Comparator<? super T> c) {
+    public static boolean checkPerformance = false;
+
+    public static List<String> fillGapsByHashSet(List<String> deps, Comparator<? super String> c) {
+        long startTime = 0;
+        if (checkPerformance) {
+            startTime = System.nanoTime();
+        }
         HashSet<String> tmp = new HashSet<>();
         for (String dep : deps) {
             String start = "";
@@ -12,11 +18,35 @@ public class Departments {
                 tmp.add(start);
             }
         }
-        return sort(new ArrayList<>(tmp), (Comparator) c);
+        List<String> list = new ArrayList<>(tmp);
+        list.sort(c);
+        if (checkPerformance) {
+            long stopTime = System.nanoTime();
+            long microsecondsElapsedTime = (stopTime - startTime) / 1000;
+            System.out.println("HashSet with comparator:" + c.getClass() + " : " + microsecondsElapsedTime);
+        }
+        return list;
     }
 
-    public static <T> List<String> sort(List<String> orgs, Comparator<? super T> c) {
-        orgs.sort((Comparator) c);
-        return orgs;
+    public static List<String> fillGapsByTreeSet(List<String> deps, Comparator<? super String> c) {
+        long startTime = 0;
+        if (checkPerformance) {
+            startTime = System.nanoTime();
+        }
+        TreeSet<String> tmp = new TreeSet<>(c);
+        for (String dep : deps) {
+            String start = "";
+            for (String el : dep.split("/")) {
+                start = start.isEmpty() ? start + el : start + "/" + el;
+                tmp.add(start);
+            }
+        }
+        List<String> list = new ArrayList<>(tmp);
+        if (checkPerformance) {
+            long stopTime = System.nanoTime();
+            long microsecondsElapsedTime = (stopTime - startTime) / 1000;
+            System.out.println("TreeSet with comparator : " + c.getClass() + " : " + microsecondsElapsedTime);
+        }
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection.departments;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Comparator;
@@ -9,27 +11,60 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class DepartmentsTest {
+
+    @Before
+    public void checkPerformanceOn() {
+        Departments.checkPerformance = true;
+    }
+
+    @After
+    public void checkPerformanceOff() {
+        Departments.checkPerformance = false;
+        System.out.println("");
+    }
+
     @Test
-    public void whenMissedAndSortNaturalOrder() {
+    public void whenMissedAndSortNaturalOrderWithHashSet() {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName());
         List<String> input = Arrays.asList("k1/sk1", "k2/sk1/ssk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk1");
-        List<String> result = Departments.fillGaps(input, Comparator.naturalOrder());
+        List<String> result = Departments.fillGapsByHashSet(input, Comparator.naturalOrder());
         assertThat(result, is(expect));
     }
 
     @Test
-    public void whenMissedAndSortReversOrder() {
+    public void whenMissedAndSortReversOrderWithHashSet() {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName());
         List<String> input = Arrays.asList("k1/sk1", "k2/sk1/ssk2", "k2/sk1/ssk1");
         List<String> expect = Arrays.asList("k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2", "k1", "k1/sk1");
-        List<String> result = Departments.fillGaps(input, new DepDescComp());
+        List<String> result = Departments.fillGapsByHashSet(input, new DepDescComp());
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMissedAndSortNaturalOrderWithTreeSet() {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName());
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1/ssk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk1");
+        List<String> result = Departments.fillGapsByTreeSet(input, Comparator.naturalOrder());
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMissedAndSortReversOrderWithTreeSet() {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName());
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1/ssk2", "k2/sk1/ssk1");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2", "k1", "k1/sk1");
+        List<String> result = Departments.fillGapsByTreeSet(input, new DepDescComp());
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName());
         List<String> input = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk1");
-        List<String> result = Departments.fillGaps(input, Comparator.naturalOrder());
+        List<String> result = Departments.fillGapsByHashSet(input, Comparator.naturalOrder());
         assertThat(result, is(expect));
     }
 }
