8625cc105163bf623c24631eca34af3bd40753ff
vooodooo222 <lazer_shtamp@mail.ru>
2020-06-08 21:16:38 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#241298]

---
+++ chapter_004/src/main/java/ru/job4j/streamapi/bank/BankService.java
@@ -0,0 +1,68 @@
+package ru.job4j.streamapi.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        List<Account> accountList = getAccountList(passport);
+        if (!accountList.contains(account)) {
+            accountList.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        User foundUser = null;
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                foundUser = user;
+                break;
+            }
+        }
+        return foundUser;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account foundAccount = null;
+        List<Account> accountList = getAccountList(passport);
+        for (Account account : accountList) {
+            if (account.getRequisite().equals(requisite)) {
+                foundAccount = account;
+                break;
+            }
+        }
+        return foundAccount;
+    }
+
+    private List<Account> getAccountList(String passport) {
+        List<Account> accountList = new ArrayList<>();
+        User user = findByPassport(passport);
+        if (user != null) {
+            accountList = users.get(user);
+        }
+        return accountList;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        if (srcAccount != null && srcAccount.getBalance() >= amount) {
+            Account dstAccount = findByRequisite(destPassport, destRequisite);
+            if (dstAccount != null) {
+                dstAccount.setBalance(dstAccount.getBalance() + amount);
+                srcAccount.setBalance(srcAccount.getBalance() - amount);
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+}
